137f83f087a9cb4a43121560d71bd307bbae486a
Marco <5439724524@student.cms.k12.nc.us>
2026-01-11 16:50:52 -0500

    update const ids

--- maincode.js
+++ maincode.js
@@ -1,2 +1,8 @@
 skins = [ "freddy", "blueberry", "snoo", ]
 //progress on skins
+document.addEventListener('DOMContentLoaded', (event) => {
+const acorntitle = document.getElementById('home');
+const characters = document.getElementById('characters');
+const play = document.getElementById('play');
+  
+                                           
